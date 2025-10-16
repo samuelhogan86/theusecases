6ab4746d63ba2d70ec2996526a2d2ef9765d91ca
Caleb Mesiti <calebmesiti555@gmail.com>
2025-10-16 13:57:31 -0400

    format blueprints
    setup basic express login/register endpoints for /login page. needs controller logic

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,1 +1,21 @@
 //Login Authentication Endpoints
+
+//Importing express
+const express = require('express');
+const router = express.Router();
+
+
+
+//Importing controllers, handles logic for routes (business logic)
+const { loginUser, registerUser } = require('../controllers/authController');
+
+//middleware can go here
+
+
+//Login Route, post request, hash pass, check db, return token
+router.post('/login', loginUser);
+
+//Register Route, post request, hash pass, store in db, return token
+router.post('/register', registerUser);
+
+module.exports = router;
