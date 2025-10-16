9b9fa084b6c2d291f887039bf52d72ea61d66ef3
samuelhogan86 <samuelhogan86@gmail.com>
2025-10-15 22:50:29 -0400

    Set up MongoDB connection, added /users and /appointments endpoints

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
+const { connectDB } = require("./config/db.js");
+require("dotenv").config();
 
 app.use(express.static("public"));
 
-app.listen(3000, function(){ 
-    console.log("Listening on port 3000");
-});
+let db;
+
+connectDB().then(database => {
+    db = database;
+    
+    app.get("/users", async (req, res) => {
+        const users = await db.collection("users").find().toArray();
+        res.json(users);
+    });
+
+    app.get("/appointments", async (req, res) => {
+        const appointments = await db.collection("appointments").find().toArray();
+        res.json(appointments);
+    });
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => 
+        console.log(`Server is running on port ${PORT}`));
+    });
