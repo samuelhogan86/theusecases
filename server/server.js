48eeee41799720a480707069509ef6c9ef53fedd
Caleb Mesiti <105940692+CMesiti@users.noreply.github.com>
2025-11-04 16:34:49 -0500

    updates to server.js BUG Fixes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 //Run this file with the command "node server.js" in your terminal
 //starts application server on port 3000
 const express = require("express");
-const mongoose = require('mongoose');
 const authRoute = require('./routes/authRoutes.js');
 const adminRoute = require('./routes/adminRoutes.js');
 const { connectDB } = require("../config/db.js");
-require("dotenv").config();
+const dotenv = require("dotenv")
 
 
+dotenv.config();
 const app = express();
 app.use(express.static("../client/public"));
 app.use(express.json());
@@ -16,8 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 let db;
 
 connectDB().then(async database => {
+    if (!database) throw new Error("Database connection failed.");
     db = database;
-
+    
     //debug code
     app.use((req, res, next) => {
         console.log(`${req.method} ${req.url}`);
@@ -29,15 +30,15 @@ connectDB().then(async database => {
     app.use(authRoute);
     app.use('/api/admin', adminRoute);
     
-    app.get("/users", async (req, res) => {
-        const users = await db.collection("users").find().toArray();
-        res.json(users);
-    });
+    // app.get("/users", async (req, res) => {
+    //     const users = await db.collection("users").find().toArray();
+    //     res.json(users);
+    // });
 
-    app.get("/appointments", async (req, res) => {
-        const appointments = await db.collection("appointments").find().toArray();
-        res.json(appointments);
-    });
+    // app.get("/appointments", async (req, res) => {
+    //     const appointments = await db.collection("appointments").find().toArray();
+    //     res.json(appointments);
+    // });
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => 
