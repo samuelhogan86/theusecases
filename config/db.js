477ad1708ec081572cbac987a62f6e215f578fb5
Caleb Mesiti <105940692+CMesiti@users.noreply.github.com>
2025-11-04 16:24:43 -0500

    mongoose db connection fixed

--- config/db.js
+++ config/db.js
@@ -4,16 +4,14 @@ const mongoose = require("mongoose");
 
 dotenv.config();
 
-const client = new MongoClient(process.env.MONGODB_URI);
-
-
 //Mongoose connection works with models lookup utilizing mongoose connection
 async function connectDB(){
   try{
-    const connection = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
-    return client.db("theusecases");
+    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "theusecases"
+    });
     
+    console.log("Connected to MongoDB");
   }catch(error){
     console.error("Error connecting to MongoDB:", error);
     throw error;
