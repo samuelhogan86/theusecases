b8727c15f0dde5c8a26db8fcd0c424c42903c871
Caleb Mesiti <105940692+CMesiti@users.noreply.github.com>
2025-11-29 17:09:15 -0500

    temp fix

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ dotenv.config();
 const authRoute = require('./routes/authRoutes.js');
 const userRoutes = require('./routes/userRoutes.js');
 const appointmentRoutes = require('./routes/appointmentRoutes.js');
+const adminRoutes = require('./routes/appointmentRoutes.js');
 const { connectDB } = require("./config/db.js");
 
 const app = express();
@@ -43,6 +44,7 @@ connectDB().then(async database => {
     app.use('/api/auth', authRoute);
     app.use('/api/appointments', appointmentRoutes);
     app.use('/api/users', userRoutes);
+    app.use('/api/admin')
     
     // app.get("/users", async (req, res) => {
     //     const users = await db.collection("users").find().toArray();
